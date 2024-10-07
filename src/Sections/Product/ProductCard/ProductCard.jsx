3bc020ee077d5797eccf2a4dc3092cebf56a5e4f
Faisal <sonalik@traffic.net.nz>
2024-10-07 22:41:27 +0530

    product card ui enhancement

--- src/Sections/Product/ProductCard/ProductCard.jsx
+++ src/Sections/Product/ProductCard/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './ProductCard.scss';
+// import './ProductCard.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../../../Store/actions/cartActions';
 import Cookies from 'js-cookie';
@@ -50,44 +50,84 @@ const ProductCard = ({ itemNum, cakeSrc, itemPrice, title, description, category
 
     const truncateDescription = (description) => {
         const words = description.split(' ');
-        return words.length > 20 ? words.slice(0, 20).join(' ') + '...' : description;
+        return words.length > 9 ? words.slice(0, 9).join(' ') + '...' : description;
     };
 
     return (
-        <div className='menuCard'>
-            <main>
-                <div className='w-full'>
-                    <img src={cakeSrc} alt={itemNum} />
+        <>
+            <section className=' h-[34rem] w-[22rem] bg-white flex flex-col justify-start items-center gap-2 p-2 rounded-xl shadow-xl'>
+                <div className='h-[20rem] w-full flex justify-center items-center'>
+                    <img width={"60%"} src={cakeSrc} alt={itemNum} />
                 </div>
-                <div className='prd-texts w-full space-y-2'>
+                <div className='h-[3rem] w-[90%]'>
                     <p className='text-gray-600'>
-                        Cake: <span className='font-semibold text-black'>{title}</span>
+                        Cake: <span className='font-semibold text-black'>{" title remove add substract addmission laptop software"}</span>
                     </p>
+                </div>
+                <div className='h-[4rem] w-[90%]'>
                     <Tooltip sx={{ background: "white", color: "black", height: "30%" }} title={description} arrow>
                         <p className='text-gray-600'>
                             Description: <span className='font-medium text-black'>{truncateDescription(description)}</span>
                         </p>
                     </Tooltip>
+                </div>
+                <div className='h-[1.5rem] w-[90%]'>
                     <p className='font-thin'>
                         Category: <strong>{category}</strong>
                     </p>
+                </div>
+                <div className='h-[1.5rem] w-[90%]'>
                     <p className='text-left text-gray-600'>
                         Price: <strong className='text-black'>${itemPrice}</strong>
                     </p>
                 </div>
-
-            </main >
-            <div className='w-full p-4'>
-                <button id='addtocartbtn'
-                    disabled={disabledCart}
-                    onClick={handleAddToCart}
-                    className="flex justify-center items-center gap-2"
-                >
-                    {disabledCart ? 'Added to Cart' : 'Add to Cart'}
-                </button>
-            </div>
-        </div >
+                <div className='h-[3rem] w-[90%] flex justify-center items-center bg-primary rounded-lg text-white'>
+                    <button id='addtocartbtn'
+                        disabled={disabledCart}
+                        onClick={handleAddToCart}
+                        className="flex justify-center items-center gap-2"
+                    >
+                        {disabledCart ? 'Added to Cart' : 'Add to Cart'}
+                    </button>
+                </div>
+            </section>
+        </>
     );
 };
 
 export default ProductCard;
+
+
+{/* <div className='menuCard'>
+    <main>
+        <div className='w-full' style={{ width: "100px", height: "100px" }}>
+            <img src={cakeSrc} alt={itemNum} />
+        </div>
+        <div className='prd-texts w-full space-y-2'>
+            <p className='text-gray-600'>
+                Cake: <span className='font-semibold text-black'>{title}</span>
+            </p>
+            <Tooltip sx={{ background: "white", color: "black", height: "30%" }} title={description} arrow>
+                <p className='text-gray-600'>
+                    Description: <span className='font-medium text-black'>{truncateDescription(description)}</span>
+                </p>
+            </Tooltip>
+            <p className='font-thin'>
+                Category: <strong>{category}</strong>
+            </p>
+            <p className='text-left text-gray-600'>
+                Price: <strong className='text-black'>${itemPrice}</strong>
+            </p>
+        </div>
+
+    </main >
+    <div className='w-full p-4'>
+        <button id='addtocartbtn'
+            disabled={disabledCart}
+            onClick={handleAddToCart}
+            className="flex justify-center items-center gap-2"
+        >
+            {disabledCart ? 'Added to Cart' : 'Add to Cart'}
+        </button>
+    </div>
+</div > */}
